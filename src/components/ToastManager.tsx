3c5d8db72cdf1c5c5a310118e9a8f52d03d856be
BoeunHan <h.boeunn@gmail.com>
2026-03-26 22:57:31 +0900

    refactor: toast item에 memo 적용

--- src/components/ToastManager.tsx
+++ src/components/ToastManager.tsx
@@ -5,6 +5,7 @@ import WarningIcon from "../icons/WarningIcon";
 import InfoIcon from "../icons/InfoIcon";
 import { useToastContext } from "../providers/ToastProvider";
 import { JSX } from "react";
+import React from "react";
 
 const ICON_MAP: Record<ToastMessageType, (props: any) => JSX.Element> = {
   success: SuccessIcon,
@@ -19,23 +20,39 @@ export const ToastManager = () => {
   return (
     <div className="toast-container">
       {toasts.map(({ id, type, message, state }) => {
-        const Icon = ICON_MAP[type];
         return (
-          <div
-            key={id}
-            className={`toast-item ${
-              state === "visible"
-                ? "toast-item-visible"
-                : state === "create"
-                ? "toast-item-create"
-                : "toast-item-hidden"
-            }`}
-          >
-            <Icon className="toast-icon" />
-            <p>{message}</p>
-          </div>
+          <ToastItem key={id} type={type} message={message} state={state} />
         );
       })}
     </div>
   );
 };
+
+const ToastItem = React.memo(
+  ({
+    type,
+    message,
+    state,
+  }: {
+    type: ToastMessageType;
+    message: string;
+    state: ToastMessageState;
+  }) => {
+    const Icon = ICON_MAP[type];
+
+    return (
+      <div
+        className={`toast-item ${
+          state === "visible"
+            ? "toast-item-visible"
+            : state === "create"
+              ? "toast-item-create"
+              : "toast-item-hidden"
+        }`}
+      >
+        <Icon className="toast-icon" />
+        <p>{message}</p>
+      </div>
+    );
+  },
+);
